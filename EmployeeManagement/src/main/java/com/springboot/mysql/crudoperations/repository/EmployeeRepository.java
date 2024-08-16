3f4272b6119c89be6319188eebdf58868100b626
Too Cool <bhupesh.shrestha19@gmail.com>
2024-08-16 10:50:31 -0700

    Commited

--- EmployeeManagement/src/main/java/com/springboot/mysql/crudoperations/repository/EmployeeRepository.java
+++ EmployeeManagement/src/main/java/com/springboot/mysql/crudoperations/repository/EmployeeRepository.java
@@ -3,7 +3,12 @@ package com.springboot.mysql.crudoperations.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.springboot.mysql.crudoperations.model.Employee;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
 
 }
+
+
+
---
+++ EmployeeManagement/src/main/java/com/springboot/mysql/crudoperations/repository/Practice.java
@@ -0,0 +1,14 @@
+package com.springboot.mysql.crudoperations.repository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Practice {
+
+
+
+    @GetMapping
+    public List<Item> getAll()
+        return
+}
