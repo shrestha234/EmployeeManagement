b7899629be89b5f58ae3b0ee89d942f5db3eaeec
shrestha234 <63519134+shrestha234@users.noreply.github.com>
2024-08-16 10:41:32 -0700

    Update EmployeeRepository.java

--- EmployeeManagement/src/main/java/com/springboot/mysql/crudoperations/repository/EmployeeRepository.java
+++ EmployeeManagement/src/main/java/com/springboot/mysql/crudoperations/repository/EmployeeRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.springboot.mysql.crudoperations.model.Employee;
 
+//OKKKK ADDED
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
 
 }
